f56230629ece26ccf86670b57ee93694bf0e5280
Praveen Balireddy <praveeniitkgp1994@gmail.com>
2019-07-26 04:54:14 +0530

    Add all files

---
+++ _site/precache-manifest.b8b83c30dca8d1b8cd4fb792e46894ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9000cee20eeb9365cbd62fe178e7d4d",
+    "url": "/static/media/Bharath Raj Resume.d9000cee.pdf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a271eb82cc135154b87a",
+    "url": "/static/js/main.a271eb82.chunk.js"
+  },
+  {
+    "revision": "20ad9f42d3689f4f2d0e",
+    "url": "/static/js/1.20ad9f42.chunk.js"
+  },
+  {
+    "revision": "a271eb82cc135154b87a",
+    "url": "/static/css/main.7ba69cb7.chunk.css"
+  },
+  {
+    "revision": "08d03993fd0af50d6517de0f681c6501",
+    "url": "/index.html"
+  }
+];
